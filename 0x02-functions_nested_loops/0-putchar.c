0937bd85cc3750aacb13df8ff6d132b53568cbb4
sirlaua <kilamgs@gmail.com>
2022-12-13 15:03:36 -0800

    putcha

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
-#include <unistd.h>
 #include "main.h"
-
 /**
- *  _putchar - writes the character c to stdout
- *  @c: The character to print
- *
- *  Return: On success 1.
- *  On error, -1 is returned, and errno is set appropriately.
+ * main - Entry Block
+ * Description: prints _putchar followed by a new line
+ * Return: 0
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	char c[8] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+
+	_putchar('\n');
+	return (0);
 }
