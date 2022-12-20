1b33b535800d1b910905920108e7604cdbe82978
sirlaua <kilamgs@gmail.com>
2022-12-20 11:21:13 -0800

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,18 +9,34 @@
  */
 int main(void)
 {
-	int n = 0, c = 0;
-	time_t t;
+	int i, j, n, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
+	srand(time(NULL));
+	while (s != 2772)
 	{
-		n = rand() % 128;
-		if ((c + n) > 2772)
-			break;
-		c = c + n;
-		printf("%c", n);
+		i = n = s = 0;
+		while ((2772 - 122) > s)
+		{
+			j = rand() % 62;
+			p[i] = c[j];
+			s += c[j];
+			i++;
+		}
+		while (c[n])
+		{
+			if (c[n] == (2772 - s))
+			{
+				p[i] = c[n];
+				s += c[n];
+				i++;
+				break;
+			}
+			n++;
+		}
 	}
-	printf("%c\n", (2772 - c));
+	p[i] = '\0';
+	printf("%s", p);
 	return (0);
 }
