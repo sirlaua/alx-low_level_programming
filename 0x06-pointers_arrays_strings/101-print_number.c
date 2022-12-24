12229619b71dc65b26137fb51aaaa0784f27e77e
sirlaua <kilamgs@gmail.com>
2022-12-23 23:38:42 -0800

    buffer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,28 +1,35 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: input integer
- * Return: void
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: void.
  */
 void print_number(int n)
 {
+	unsigned int i, m, c;
+
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		_putchar(45);
+		i = n * -1;
 	}
-	print_uint((unsigned int) n);
-}
+	else
+	{
+		i = n;
+	}
 
-/**
-* print_uint - Prints an unsigned integer
-* @n: The unsigned int to print
-*/
+	m = i;
+	c = 1;
 
-void print_uint(unsigned int n)
-{
-	if (n / 10 != 0)
-	print_uint(n / 10);
-	_putchar(n % 10 + '0');
+	while (m > 9)
+	{
+		m /= 10;
+		c *= 10;
+	}
+
+	for (; c >= 1; c /= 10)
+	{
+		_putchar(((i / c) % 10) + 48);
+	}
 }
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - function prints a buffer
+ * @b: buffer
+ * @size: size of buffer
+ * Return: void.
+ */
+void print_buffer(char *b, int size)
+{
+	int n, j, k;
+
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (n = 0; i n size; n += 10)
+		{
+			printf("%.8x:", n);
+			for (j = n; j < n + 10; j++)
+			{
+				if (j % 2 == 0)
+					printf(" ");
+				if (j < size)
+					printf("%.2x", *(b + j));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (k = n; k < n + 10; k++)
+			{
+				if (k >= size)
+					break;
+				if (*(b + k) < 32 || *(b + k) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + k));
+			}
+			printf("\n");
+		}
+	}
+}
