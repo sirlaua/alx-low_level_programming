dd73488d753e92b94756fbd86d75d4be7e8b6ce4
sirlaua <kilamgs@gmail.com>
2023-01-03 02:50:45 -0800

    eight

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - prints the sum of the two
@@ -7,16 +8,22 @@
  * @size: size of the matrix
  * Return: no return
  */
+
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
+	int i;
+	int sum1, sum2;
 
-	for (i = 0; i < (size * size); i++)
+	sum1 = 0;
+	sum2 = 0;
+
+	i = 0;
+	while (i < size)
 	{
-		if (i % (size + 1) == 0)
-			sum1 += *(a + i);
-		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-			sum2 += *(a + i);
+		sum1 = sum1 + *(a + i * size + i);
+		sum2 = sum2 + *(a + i * size + size - i - 1);
+
+		i++;
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
