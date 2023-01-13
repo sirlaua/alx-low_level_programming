cd9f7ea1b8ec7c0e05942924d35381d565ad544c
sirlaua <kilamgs@gmail.com>
2023-01-13 06:02:22 -0800

    more malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ *
+ * @nmemb: number of elements
+ * @size: memory size in bytes
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	a = malloc(nmemb * size);
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		a[i] = 0;
+
+	return (a);
+}
